79bd398cabcbdd20c3fb7020055c87553f0683a1
Ulises Romero <uromero@garbarino.com>
2021-08-16 14:58:13 -0300

    Agrego servicios para crud

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,7 +1,37 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/demo")
 @Controller
 public class CustomerController {
+
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping(value = "/customers")
+    public String getCustomers() {
+        return this.customerService.getCustomers();
+    }
+
+    @PostMapping(value = "/customers")
+    public String createCustomer() {
+        return "";
+    }
+
+    @PutMapping(value = "/customers")
+    public String updateCustomer() {
+        return "";
+    }
+
 }
--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -1,7 +1,25 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Customer;
+import com.example.demo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public String getCustomers() {
+        List<Customer> customer = customerRepository.findAll();
+        return "";
+    }
+
 }
